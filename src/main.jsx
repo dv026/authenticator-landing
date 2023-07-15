58a5092f3d4c59d376f79e22e1d2d382802921e0
Dmitrii Vasilev <dimavas026@gmail.com>
2023-07-15 15:54:14 +0300

    add router

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter, Navigate, Route, Router, Routes } from 'react-router-dom';
 
 import ReactDOM from 'react-dom/client';
 import App from './App';
@@ -6,6 +7,11 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<App />} />
+        <Route path='/*' element={<Navigate to='/' />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
