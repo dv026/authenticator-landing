211b643ca2b91afdf0db1d262889d309f46629e3
Dmitrii Vasilev <dimavas026@gmail.com>
2023-07-15 16:38:08 +0300

    change address

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Navigate, Route, Router, Routes } from 'react-router-dom';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 
 import ReactDOM from 'react-dom/client';
 import App from './App';
